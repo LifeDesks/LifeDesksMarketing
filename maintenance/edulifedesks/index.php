4ff455635b4acde79d3d201214aa8e6b993b74e1
Lisa Walley <lwalley@eol.org>
2011-12-28 14:46:36 -0500

    Recovered edulifedesks maintenance page and moved it from edulifedesks folder to maintenance folder to avoid any further conflicts and subsequent disappearances.

---
+++ maintenance/edulifedesks/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+
+<head>
+  <title>EduLifeDesks is offline for maintenance</title>
+  <link href="css/learning.css" type="text/css" media="all" rel="stylesheet"/>
+  <meta name="robots" content="noindex, nofollow" />
+</head>
+<body>
+  <div id="page"><div id="page-inner">
+    <div id="header">
+      <div id="logo-title">
+        <img id="logo" alt="Encyclopedia of Life logo" src="images/logo.gif" />
+        <h1>Education LifeDesks</h1>
+        <div id="site-slogan">Encyclopedia of Life tools for educators, students and enthusiasts worldwide.</div>
+      </div>
+    </div>
+    <div id="main"><div id="main-inner">
+      <div id="content"><div id="content-inner">
+        <h2>Under Maintenance</h2>
+        <p>We're sorry, Education LifeDesks are currently undergoing routine maintenance. We will be back online shortly.</p>
+        <p>Education LifeDesks are brought to you by the <a href="http://www.eol.org">Encyclopedia of Life (EOL)</a>, for more information please visit the <a href="http://education.eol.org">Learning + Education Group</a>.</p>
+      </div></div>
+    </div></div>
+    <div id="footer">
+      <p>Encyclopedia of Life Learning + Education Group | Museum of Comparative Zoology | Harvard University 26 Oxford Street, Cambridge, MA 02318 USA | 617-496-6764 | education [at] eol.org</p>
+    </div>
+  </div></div>
+</body>
+</html>
